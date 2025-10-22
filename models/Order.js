251a0720d0c4aca3381ce7cd82a072bced315727
bagotsaycg <bagotsaycg@gmail.com>
2025-10-22 15:46:05 +0800

    Create order schema

--- models/Order.js
+++ models/Order.js
@@ -1,1 +1,44 @@
-order.js
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+    
+    userId: {
+		type: String,
+		required: [true, 'User ID is Required']
+	},
+
+	productsOrdered: [
+		{
+			productId: {
+				type: String,
+				required: [true, 'Product ID is required']
+			},
+			quantity: {
+				type: Number,
+				required: [true, 'Quantity is required']
+			},
+			subtotal: {
+				type: Number,
+				required: [true, 'Subtotal is required']
+			}
+		}
+	],
+
+	totalPrice: {
+        type: Number,
+        required: [true, 'Total Price is required'],
+    },
+
+    orderedOn: {
+        type: Date,
+        default: Date.now
+    },
+
+    status: {
+		type: String,
+		default: 'Pending'
+	}
+});
+
+module.exports = mongoose.model('Order', orderSchema);
+
